0128a8c1d67ef1b25441fb3747647a2e0cfe22cc
Garulf <535299+Garulf@users.noreply.github.com>
2022-03-20 18:40:21 -0400

    init

---
+++ plugin/main.py
@@ -0,0 +1,54 @@
+from flox import Flox, ICON_APP_ERROR, FLOW_API, WOX_API
+
+import wallpaper_engine as we
+from wallpaper_engine import PATH, EXE32
+
+class WallpaperEngineProfileSelector(Flox):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.config_path = self.settings.get('config_path', PATH)
+        self.exe = self.settings.get('exe', EXE32)
+        self.config = we.get_config(self.config_path)
+
+    def query(self, query):
+        try:
+            profiles = we.get_profiles(self.config_path, self.exe)
+            if len(profiles) == 0:
+                self.add_item(
+                    title='No profiles found',
+                    subtitle='Please create a profile in Wallpaper Engine.',
+                    icon=ICON_APP_ERROR
+                )
+            for profile in profiles:
+                if query.lower() in profile.name.lower():
+                    self.add_item(
+                        title=profile.name,
+                        subtitle='Set Wallpaper Engine to use this profile.',
+                        icon=self.icon,
+                        method=self.select_profile,
+                        parameters=[profile.name]
+                    )
+        except FileNotFoundError:
+            self.add_item(
+                title='Wallpaper Engine not found',
+                subtitle='Please provide the path to Wallpaper Engine in settings.',
+                icon=ICON_APP_ERROR,
+                method=self.open_setting_dialog
+            )
+
+    def context_menu(self, data):
+        profile_name = data[0]
+        self.add_item(
+            title=f'Set the wallpaper profile to {profile_name}',
+            subtitle='Sets Wallpaper Engine to use this profile.',
+            icon=self.icon,
+            method=self.select_profile,
+            parameters=[profile_name]
+        )
+
+    def select_profile(self, profile_name):
+        profiles = we.set_profile(profile_name, self.config_path, self.exe)
+
+if __name__ == "__main__":
+    WallpaperEngineProfileSelector()
---
+++ plugin/wallpaper_engine.py
@@ -0,0 +1,59 @@
+from codecs import ignore_errors
+import json
+from pathlib import Path
+from subprocess import Popen, PIPE
+
+
+PATH = 'C:\Steam\steamapps\common\wallpaper_engine'
+CONFIG_FILE = 'config.json'
+EXE32 = 'wallpaper32.exe'
+EXE64 = 'wallpaper64.exe'
+HIDE_WINDOW = 0x08000000
+
+
+def get_config(path=PATH):
+    path = str(path)
+    full_path = Path(path, CONFIG_FILE)
+    with open(full_path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def get_profiles(wallpaper_engine_path, exe=EXE32):
+    config = get_config(wallpaper_engine_path)
+    profiles = []
+    for key in config.keys():
+        if key == '?installdirectory' or key == '_installdirectory':
+            continue
+        else:
+            user = config[key]
+    for profile in user['general'].get('profiles', []):
+        profiles.append(Profile(profile, wallpaper_engine_path))
+    return profiles
+
+def cmd(command):
+    return Popen(command, stdout=PIPE, stderr=PIPE, shell=True, creationflags=HIDE_WINDOW)
+
+def set_profile(profile_name, wallpaper_engine_dir, exe):
+    exe_path = Path(wallpaper_engine_dir, exe)
+    args = f'{exe_path} -control openProfile -profile "{profile_name}"'
+    cmd(args)
+
+class WallpaperEngine(object):
+
+    def __init__(self, wallpaper_engine_dir, exe=EXE32):
+        self.wallpaper_engine_dir = wallpaper_engine_dir
+        self.exe = exe
+        self.config = get_config(self.wallpaper_engine_dir)
+        self.profiles = get_profiles(self.wallpaper_engine_dir, self.exe)
+
+class Profile(object):
+
+    def __init__(self, data, wallpaper_engine_path, exe=EXE32):
+        self.data = data
+        self.wallpaper_engine_path = wallpaper_engine_path
+        self.exe = exe
+        for k, v in data.items():
+            setattr(self, k, v)
+
+    def set(self):
+        set_profile(self.name, self.wallpaper_engine_path, self.exe)
+            
