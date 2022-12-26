5487c8523a7dd0cc16a9889bcc1c194d27bc04cf
Garulf <535299+Garulf@users.noreply.github.com>
2022-12-26 12:29:30 -0500

    Select first user config instead of iterating

--- plugin/wallpaper_engine.py
+++ plugin/wallpaper_engine.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from subprocess import Popen, PIPE
 
 
-PATH = 'C:\Steam\steamapps\common\wallpaper_engine'
+PATH = r'E:\SteamLibrary\steamapps\common\wallpaper_engine'
 CONFIG_FILE = 'config.json'
 EXE32 = 'wallpaper32.exe'
 EXE64 = 'wallpaper64.exe'
@@ -17,25 +17,22 @@ def get_config(path=PATH):
     with open(full_path, 'r', encoding='utf-8') as f:
         return json.load(f)
 
+
 def get_profiles(wallpaper_engine_path, exe=EXE32):
     config = get_config(wallpaper_engine_path)
-    profiles = []
-    for key in config.keys():
-        if key == '?installdirectory' or key == '_installdirectory':
-            continue
-        else:
-            user = config[key]
-    for profile in user['general'].get('profiles', []):
-        profiles.append(Profile(profile, wallpaper_engine_path))
-    return profiles
+    user_key = list(config.keys())[1]
+    return [Profile(p, wallpaper_engine_path, exe) for p in config[user_key]['general'].get('profiles', [])]
+
 
 def cmd(command):
     return Popen(command, stdout=PIPE, stderr=PIPE, shell=True, creationflags=HIDE_WINDOW)
+
 
 def set_profile(profile_name, wallpaper_engine_dir, exe):
     exe_path = Path(wallpaper_engine_dir, exe)
     args = f'{exe_path} -control openProfile -profile "{profile_name}"'
     cmd(args)
+
 
 class WallpaperEngine(object):
 
@@ -43,7 +40,9 @@ class WallpaperEngine(object):
         self.wallpaper_engine_dir = wallpaper_engine_dir
         self.exe = exe
         self.config = get_config(self.wallpaper_engine_dir)
-        self.profiles = get_profiles(self.wallpaper_engine_dir, self.exe)
+        self.profiles = get_profiles(
+            self.wallpaper_engine_dir, self.exe)
+
 
 class Profile(object):
 
@@ -56,4 +55,3 @@ class Profile(object):
 
     def set(self):
         set_profile(self.name, self.wallpaper_engine_path, self.exe)
-            
